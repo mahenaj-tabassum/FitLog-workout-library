52294f81be6be9d1fee8e0fc72edb3634078accd
mahenajT <mahenaj.dev@gmail.com>
2026-09-24 18:43:21 +0600

    Navbar and Footer added

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-[#090A0D] h-25 border-t border-t-[#1A1D24]">
+      <div className="flex h-full w-[85%] mx-auto items-center justify-between">
+        <Image src="/nav-logo.png" width={85} height={30} alt="Footer Logo" />
+        <p className="text-[12px] text-[#6B7280]">
+          © 2026 FitLog — Workout Library. Train hard, log honest.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -1,8 +1,54 @@
-
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="border-b sticky top-0 bg-[#0B0D12] z-50 border-b-[#1C1F26]">
+      <div className="flex justify-between items-center h-20 w-[85%] mx-auto">
+        <Image src="/nav-logo.png" width={93.41} height={30} alt="Nav Logo" />
 
-export default Navbar
+        <div className="flex items-center gap-5">
+          <Link
+            className={`${
+              pathname === "/"
+                ? "px-5 py-2 bg-[#1A2312] rounded-full text-accent"
+                : "text-body hover:bg-[#1A2312]/50 hover:text-white"
+            } rounded-full px-5 py-2 transition-all duration-300 ease-in-out  text-[15px]`}
+            href="/"
+          >
+            Workouts
+          </Link>
+          <Link
+            className={`${
+              pathname === "/my-plan"
+                ? "px-5 py-2 bg-[#1A2312] rounded-full text-accent"
+                : "text-body hover:bg-[#1A2312]/50 hover:text-white"
+            } rounded-full px-5 py-2 transition-all duration-300 ease-in-out  text-[15px]`}
+            href="/my-plan"
+          >
+            My Plan
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <button className="cursor-pointer flex items-center gap-3">
+            <Link href="/my-plan">Plan</Link>
+            <span className="inline-flex items-center justify-center text-[12px] text-black h-6 w-6 rounded-full bg-accent">
+              0
+            </span>
+          </button>
+          <button className="cursor-pointer flex items-center gap-3">
+            <Link href="/my-plan">Saved</Link>
+            <span className="inline-flex items-center justify-center shrink-0 text-[12px]  h-6 w-6 rounded-full border border-[#2D313B]">
+              0
+            </span>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
